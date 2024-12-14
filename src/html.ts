64f9fcfe28143f9af3de9dd4a399f572b7b742d0
Max Milton <max@happyhackers.xyz>
2024-12-14 23:54:50 +0900

    feat: Add new `html` test utils

---
+++ src/html.ts
@@ -0,0 +1,81 @@
+import { type Node, SyntaxKind, parse, walk } from '@maxmilton/html-parser/src';
+
+// https://html.spec.whatwg.org/multipage/syntax.html#void-elements
+const SELF_CLOSING_TAGS = new Set([
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+  '!doctype',
+  '',
+  '!',
+  '!--',
+]);
+
+interface ValidationError {
+  message: string;
+  details?: unknown;
+}
+
+// TODO: Validate attributes.
+// TODO: Validate tag names?
+// TODO: Validate void elements have no children.
+
+export function validate(html: string): {
+  valid: boolean;
+  errors: ValidationError[];
+  ast: Node[];
+} {
+  try {
+    const errors: ValidationError[] = [];
+    const ast = parse(html);
+    let hasTags = false;
+
+    walk(ast, {
+      enter(node) {
+        if (node.type !== SyntaxKind.Tag) return;
+        hasTags = true;
+
+        if (node.close === null && !SELF_CLOSING_TAGS.has(node.name)) {
+          errors.push({
+            message: 'Unclosed tag',
+            details: node.open,
+          });
+        }
+      },
+    });
+
+    if (ast.length === 0) {
+      errors.push({
+        message: 'No HTML content found',
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    } else if (!hasTags) {
+      errors.push({
+        message: 'No tags found',
+      });
+    }
+
+    return {
+      valid: errors.length === 0,
+      errors,
+      ast,
+    };
+  } catch (error) {
+    return {
+      valid: false,
+      errors: [{ message: 'Parse error', details: error }],
+      ast: [],
+    };
+  }
+}
